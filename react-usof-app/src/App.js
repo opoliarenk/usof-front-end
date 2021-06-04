1d2c02d28796a62a27bfae9b61e873411ff3889f
Olena Poliarenko <opoliarenk@e1r3p8.unit.ua>
2021-06-04 14:58:52 +0300

    get post trauble

--- react-usof-app/src/App.js
+++ react-usof-app/src/App.js
@@ -1,12 +1,40 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
 
-function App() {
+const App = () => {
+
+    const login = async () => {
+        const res = await fetch('http://localhost:3000/api/post/')
+
+        const data = await res.json();
+
+        console.log(data);
+    }
+
+
+    // const login = async (user) => {
+    //     const res = await fetch('http://localhost:3000/api/auth/login', {
+    //         method: 'POST',
+    //         body: JSON.stringify(user),
+    //     })
+    //
+    //     const data = await res.json();
+    //
+    //     console.log(data);
+    // }
+
   return (
-    <div className="container">
-      <SignIn />
-      <SignUp />
-    </div>
+      <Router>
+          <Route path="/api/auth">
+              <div className="container">
+                  <SignIn loginUser={login}/>
+                  <SignUp/>
+              </div>
+          </Route>
+      </Router>
   );
 }
 
--- react-usof-app/src/components/SignIn.js
+++ react-usof-app/src/components/SignIn.js
@@ -1,63 +1,49 @@
 import '../App.css'
 import React from 'react';
+import {useState} from 'react';
 
-const SignIn = () => {
-    return (
-        <SignInForm />
-    );
-}
+const SignIn = ({loginUser}) => {
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
 
-class SignInForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            login: '',
-            password: '',
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        if (!login) {
+            alert('please enter login');
+            return;
         }
 
-        this.changeLogin = this.changeLogin.bind(this);
-        this.changePassword = this.changePassword.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+       // loginUser({login, password});
+        loginUser();
+
+        setLogin('');
+        setPassword('')
     }
 
-    changeLogin(e) {
-        this.setState({login: e.target.value});
-    }
-
-    changePassword(e) {
-        this.setState({password: e.target.value});
-    }
-
-    handleSubmit(e) {
-        console.log(`in your login - ${this.state.login}`);
-        console.log(`in your password - ${this.state.password}`);
-        e.preventDefault();
-    }
-
-    render() {
-        return (
-            <div className="sign in">
-                <ul>
-                    <li><a className="active" href="#home">Home</a></li>
-                    <li><a href="#news">News</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                    <li><a href="#about">About</a></li>
-                </ul>
-                <div className="centered">
-                    <p>Sign in</p>
-                    <form className="signIn">
-                        <p>login</p>
-                        <input type="text" name="login" placeholder="enter your login" value={this.state.login} onChange={this.changeLogin}/>
-                        <p>password</p>
-                        <input type="password" name="password" placeholder="enter your password" value={this.state.password} onChange={this.changePassword}/>
-                        <p></p>
-                        <input type="button" name="send" value="Send" onClick={this.handleSubmit}/>
-                    </form>
-                </div>
+    return (
+        <div className="sign in">
+            <ul>
+                <li><a className="active" href="#home">Home</a></li>
+                <li><a href="#news">News</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li><a href="#about">About</a></li>
+            </ul>
+            <div className="centered">
+                <p>Sign in</p>
+                <form className="signIn">
+                    <p>login</p>
+                    <input type="text" name="login" placeholder="enter your login" value={login} onChange={(e) =>
+                    setLogin(e.target.value)}/>
+                    <p>password</p>
+                    <input type="password" name="password" placeholder="enter your password" value={password} onChange={(e) =>
+                    setPassword(e.target.value)}/>
+                    <p></p>
+                    <input type="button" name="send" value="Send" onClick={onSubmit}/>
+                </form>
             </div>
-        );
-    }
+        </div>
+    );
 }
-
 
 export default SignIn;
